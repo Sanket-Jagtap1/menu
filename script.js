747f655be206681c85383602980a801536685b3c
Sanket-Jagtap1 <78812292+Sanket-Jagtap1@users.noreply.github.com>
2021-10-14 16:50:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -28,6 +28,21 @@ let VegMenu=[
     {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
     {name: "Spcl.Crab Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"}
 ]
+let starterMenu=[
+    {name: "Masvadi Thali", Price: 300, desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "pithla bhakar", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Mutton Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"},
+    {name: "Spcl.Crab Thali", Price: 300,desc:"Unlimited rassa, Bhakari, Rice"}
+]
 
 // non veg menu function
 function loadNonVegMenu(nonVegMenu){
@@ -50,7 +65,8 @@ function loadNonVegMenu(nonVegMenu){
                     <span class="border border-dark p-1 me-3"><i class="fas fa-circle text-danger"></i></span>
                     <span class="text-success">&#8377;${menu.Price}</span>
                     </div>
-                            <input type="button" value="Order" class="btn border rounded-0">
+                    
+                    <a href="tel:8806328987" class="btn border rounded-0">order</a>
                             </div>
                             </div>
                 </div>
@@ -86,7 +102,7 @@ function loadVegMenu(VegMenu){
                                 <span class="border border-dark p-1 me-3"><i class="fas fa-circle text-success"></i></span>
                                 <span class="text-success">&#8377;${menu.Price}</span>
                             </div>
-                            <input type="button" value="Order" class="btn border rounded-0">
+                            <a href="tel:8806328987" class="btn border rounded-0">order</a>
                         </div>
                     </div>
                 </div>
@@ -98,3 +114,41 @@ function loadVegMenu(VegMenu){
 }
 
 loadVegMenu(VegMenu)
+// starter menu function
+
+
+function loadStarterMenu(starterMenu){
+    body = document.getElementById('starter');
+    data = '';
+
+    for(let menu of starterMenu){
+        data += `
+        <!-- card start -->
+                <div class="menu-card col-12 col-md-6 col-lg-3 text-center">
+                    <div class="card-content bg-white">
+                    
+                        <div class="img-box mb-2">
+                        <img src="https://im.whatshot.in/img/2018/Aug/nonvegthali-cropped-2-1533297391.jpg" class="img-fluid" alt="">
+                    </div>
+                    <h2 class="fs-4 fw-bold">${menu.name}</h2>
+                       <small class="text-muted"> [${menu.desc}]</small>
+                        
+                       <div class="d-flex justify-content-between align-items-center px-3 py-2">
+                            <div>
+                                <span class="border border-dark p-1 me-3"><i class="fas fa-circle text-success"></i></span>
+                                <span class="text-success">&#8377;${menu.Price}</span>
+                            </div>
+                            <a href="tel:8806328987" class="btn border rounded-0">order</a>
+
+                           
+                        </div>
+                    </div>
+                </div>
+                <!-- card end -->
+        `
+        console.log(data)
+        body.innerHTML = data;
+    }
+}
+
+loadStarterMenu(starterMenu)
